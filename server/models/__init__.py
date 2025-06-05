8e68b007667bdf17872c2cabc9d33ea73b35f780
brandon-bwoni <bwonibrandon@gmail.com>
2025-06-05 13:51:56 +0200

    initial agent flow

---
+++ server/models/__init__.py
@@ -0,0 +1,17 @@
+from .schema import (
+    BookingBase,
+    BookingCreate,
+    BookingUpdate,
+    BookingInDB,
+    BookingStatus,
+    PyObjectId
+)
+
+__all__ = [
+    "BookingBase",
+    "BookingCreate",
+    "BookingUpdate",
+    "BookingInDB",
+    "BookingStatus",
+    "PyObjectId"
+]
--- server/tools/__init__.py
+++ server/tools/__init__.py
@@ -1,9 +1,9 @@
 from .create_booking_tool import create_and_save_booking
 from .read_slots_tool import check_availability_and_suggest_slot
-from .update_booking_tool import update_booking_status_tool
+from .update_booking_tool import update_booking_status
 
 __all__ = [
     "create_and_save_booking",
     "check_availability_and_suggest_slot",
-    "update_booking_status_tool"
+    "update_booking_status"
 ]
